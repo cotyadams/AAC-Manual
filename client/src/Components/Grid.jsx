f8b03fbf86f7fe4b5d51e3a476af7d7ee1501a96
cotyadams <cotyadams1@gmail.com>
2026-08-30 04:36:30 +0000

    filtering home screen for top level

--- client/src/Components/Grid.jsx
+++ client/src/Components/Grid.jsx
@@ -22,8 +22,8 @@ export default function Grid({
 }) {
   return (
     <div className="grid">
-      {data.map(node => (
-        <GridNode
+      {data.map(node => {
+        if (node.topLevel && oldData.length == 0) return (<GridNode
           node={node} key={node.id}
           setTTsContent={setTTsContent} ttsContent={ttsContent}
           isSelectLeaf={isSelectLeaf} setIsSelectLeaf={setIsSelectLeaf}
@@ -32,8 +32,10 @@ export default function Grid({
           addChild={addChild} setAddChild={setAddChild}
           data={data} setData={setData}
           oldData={oldData} setOldData={setOldData}
-        />
-      ))}
+        />)
+      }
+        
+      )}
     </div>
   );
 }
